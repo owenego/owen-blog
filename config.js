21e779062abca82f53269a69b70ee0c6d6313340
owenego <55136950+owenego@users.noreply.github.com>
2019-12-18 19:32:49 +0000

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    'secret':'owen980828',
-    //'database':`${process.env.MONGO_URI}${process.env.MONGO_DB}`
-    'database': 'mongodb+srv://Owen:owenowen98828@cluster0-h7uy2.mongodb.net/wit-Blog-Owen'
-}
